828c6528dca99f69bd546ce7369fda10dbf1c74d
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-10-06 14:21:08 +0600

    update book card

--- src/pages/book/Book.jsx
+++ src/pages/book/Book.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
 
 const Book = ({ singleBook }) => {
   console.log(singleBook);
-  const {bookId, author, bookName, image, category, review, tags } = singleBook;
+  const {bookId, author, bookName, image, category, review, tags, rating } = singleBook;
   
   return (
-    <div id={bookId} className="card bg-base-100 shadow-lg">
-      <figure className="w-50 mx-auto">
-        <img
+    <div id={bookId} className="card bg-base-100 shadow-lg border border-gray-200 cursor-pointer hover:scale-105">
+      <figure className="mx-auto pt-3 h-[300px] object-cover">
+        <img className="w-50 mx-auto h-full"
           src={image}
           alt="Shoes"
         />
@@ -16,6 +17,7 @@ const Book = ({ singleBook }) => {
         <h2 className="card-title">
           {bookName}
           <div className="badge badge-secondary">{category}</div>
+          <div className="flex gap-1 items-center">{rating} <FaStar fill="orange" /></div>
         </h2>
         <p>{review.split('.').slice(0,2).join('.') + '.'}</p>
         <h3 className="font-semibold text-lg">{author}</h3>
